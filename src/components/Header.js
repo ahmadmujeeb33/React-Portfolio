8050cc83271d03d8f5dd0f77528532fded8ea96a
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2021-07-19 21:23:58 -0400

    header partilly completed

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import '../styles/header.css';
+
+function Header() {
+    return (
+      <div>
+          <div className = "menu-bar">
+            <div className = "menu-bar-items">
+                <p>About Me</p>
+                <p>Potfilio</p>
+                <p>Contact</p>
+                <p>Resume</p>
+            </div>
+          </div>
+      </div>
+    );
+  }
+  
+  export default Header;
