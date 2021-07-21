5a0d5df564dd6a92f96c3884bb2ac598b840f4d7
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2021-07-21 00:33:20 -0400

    About Me page complete

--- src/pages/AboutMePage.js
+++ src/pages/AboutMePage.js
@@ -1,5 +1,16 @@
 import '../styles/AboutMe.css';
 import avatar from "../Images/Avatar.PNG"
+import python from "../Images/python.png"
+import java from "../Images/java.png"
+import cplusplus from "../Images/c++.png"
+import javascript from "../Images/javascript.png"
+import jquery from "../Images/jquery.png"
+import node from "../Images/node.PNG"
+import react from "../Images/react.png"
+import express from "../Images/express.png"
+import mysql from "../Images/mysql.png"
+import database from "../Images/database.png"
+
 
 function AboutMePage (){
     return (
@@ -15,6 +26,30 @@ function AboutMePage (){
                         </span>
                     </div>
                 </div>
+
+                <h3>Skill Set</h3>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-sm">
+                                <h5 className = "languages">Languages</h5>
+                                <img className = "images" src = {python} width = "50" height = "50"/>
+                                <img className = "images" src = {java} width = "50" height = "50"/>
+                                <img className = "images" src = {cplusplus} width = "50" height = "50"/>
+                                <img className = "images" src = {javascript} width = "50" height = "50"/>
+                            </div>
+                            <div class="col-sm">
+                                <h5>Frameworks and Library</h5>
+                                <img src = {jquery} width = "50" height = "50"/>
+                                <img src = {node} width = "60" height = "50"/>
+                                <img src = {react} width = "60" height = "50"/>
+                                <img src = {express} width = "90" height = "70"/>
+                            </div>
+                            <div class="col-sm">
+                                <h5>Databases</h5>
+                                <img src = {database} width = "150" height = "100"/>
+                            </div>
+                        </div>
+                    </div>           
             </div>
         </div>
 
